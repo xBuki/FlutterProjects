a50c4392ee19e110a824dd4e73124d6c127434ca
Buki <70242366+xBuki@users.noreply.github.com>
2023-12-24 16:02:34 -0800

    Interacting with Flutter and Widgets.

--- dice_roll_project/lib/main.dart
+++ dice_roll_project/lib/main.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   // Custom Instruction
-  runApp(MaterialApp()); // A core function in Dart - Execution.
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 187, 40, 29),
+        body: Center(
+          child: Text('Hello Flutter'),
+        ),
+      ),
+    ),
+  ); // A core function in Dart - Execution.
 }
